d94d1ec06f8927edc2ea5ea60cc6d318e13bbe03
HevenHoo <hhfztl@163.com>
2019-12-21 10:58:03 +0800

    commit message

---
+++ SimpleFramework.framework/Headers/SimpleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SimpleFramework.h
+//  SimpleFramework
+//
+//  Created by feng on 2019/12/19.
+//  Copyright © 2019 Feng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleFramework.
+FOUNDATION_EXPORT double SimpleFrameworkVersionNumber;
+
+//! Project version string for SimpleFramework.
+FOUNDATION_EXPORT const unsigned char SimpleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleFramework/PublicHeader.h>
+
+
